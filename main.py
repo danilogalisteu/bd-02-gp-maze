a6246db50a1d0ff06e033743aa0ad3fabe8050f8
danilogalisteu <danilogalisteu@gmail.com>
2024-02-14 19:33:19 -0300

    basic window

---
+++ main.py
@@ -0,0 +1,30 @@
+
+from tkinter import Tk, Canvas
+
+
+
+class Window():
+    def __init__(self, width, height):
+        self.window = Tk()
+        self.window.title("boot.dev maze")
+        self.window.geometry(f"{width}x{height}")
+        self.window.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas = Canvas(self.window)
+        self.canvas.pack()
+        self.running = False
+    
+    def redraw(self):
+        self.window.update_idletasks()
+        self.window.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+
+win = Window(800, 600)
+win.wait_for_close()
