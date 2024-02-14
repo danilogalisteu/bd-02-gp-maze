0aa8ae528c2d499bc4623c1eeadd5b5ad0e63225
danilogalisteu <danilogalisteu@gmail.com>
2024-02-14 20:27:29 -0300

    method to draw lines on window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 from tkinter import Tk, Canvas
+from graphics import Point, Line
 
 
 
@@ -9,7 +10,7 @@ class Window():
         self.window.title("boot.dev maze")
         self.window.geometry(f"{width}x{height}")
         self.window.protocol("WM_DELETE_WINDOW", self.close)
-        self.canvas = Canvas(self.window)
+        self.canvas = Canvas(self.window, width=width, height=height)
         self.canvas.pack()
         self.running = False
     
@@ -24,7 +25,17 @@ class Window():
 
     def close(self):
         self.running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
 
 
 win = Window(800, 600)
+
+l1 = Line(Point(600, 10), Point(50, 450))
+win.draw_line(l1, 'red')
+
+l2 = Line(Point(10, 10), Point(700, 500))
+win.draw_line(l2, 'blue')
+
 win.wait_for_close()
