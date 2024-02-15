15bbb4d5f3c706ebf13b5900b212441beab105ec
danilogalisteu <danilogalisteu@gmail.com>
2024-02-14 23:36:58 -0300

    test drawing maze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 from tkinter import Tk, Canvas
-from graphics import Point, Line
-from cell import Cell
-
+from maze import Maze
 
 
 class Window():
@@ -33,31 +31,6 @@ class Window():
 
 win = Window(800, 600)
 
-l1 = Line(Point(600, 10), Point(50, 450))
-win.draw_line(l1, 'red')
-
-l2 = Line(Point(10, 10), Point(700, 500))
-win.draw_line(l2, 'blue')
-
-c1 = Cell(200, 200, 250, 250, win)
-c1.draw()
-
-c2 = Cell(300, 300, 350, 350, win)
-c2.has_bottom_wall = False
-c2.draw()
-
-c3 = Cell(400, 400, 450, 450, win)
-c3.has_top_wall = False
-c3.has_left_wall = False
-c3.draw()
-
-c4 = Cell(500, 500, 550, 550, win)
-c4.has_right_wall = False
-c4.has_left_wall = False
-c4.draw()
-
-c1.draw_move(c2)
-
-c3.draw_move(c4, undo=True)
+m = Maze(50, 50, 10, 10, 50, 50, win)
 
 win.wait_for_close()
