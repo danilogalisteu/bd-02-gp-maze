0b398c248b30397e40b15f5541924ed9d532fcc6
danilogalisteu <danilogalisteu@gmail.com>
2024-02-14 21:45:04 -0300

    test drawing cells

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from tkinter import Tk, Canvas
-from graphics import Point, Line
+from graphics import Point, Line, Cell
 
 
 
@@ -37,5 +37,22 @@ win.draw_line(l1, 'red')
 
 l2 = Line(Point(10, 10), Point(700, 500))
 win.draw_line(l2, 'blue')
+
+c1 = Cell(200, 200, 250, 250, win)
+c1.draw()
+
+c2 = Cell(300, 300, 350, 350, win)
+c2.has_bottom_wall = False
+c2.draw()
+
+c3 = Cell(400, 400, 450, 450, win)
+c3.has_top_wall = False
+c3.has_left_wall = False
+c3.draw()
+
+c4 = Cell(500, 500, 550, 550, win)
+c4.has_right_wall = False
+c4.has_left_wall = False
+c4.draw()
 
 win.wait_for_close()
