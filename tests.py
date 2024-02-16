d2c1d8f62fbdb3a57139930e9623df22de43b8a1
danilogalisteu <danilogalisteu@gmail.com>
2024-02-16 10:16:12 -0300

    added test for entrance and exit wall openings

--- tests.py
+++ tests.py
@@ -32,18 +32,22 @@ class Tests(unittest.TestCase):
             num_rows,
         )
 
+    def test_maze_entrance_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertFalse(
+            m1._cells[0][0].has_top_wall,
+        )
+        self.assertFalse(
+            m1._cells[num_cols-1][num_rows-1].has_bottom_wall,
+        )
+
     def test_maze_clear_visited(self):
         num_cols = 14
         num_rows = 10
         m1 = Maze(50, 50, num_rows, num_cols, 50, 50, seed=42)
-        self.assertEqual(
-            len(m1._cells),
-            num_cols,
-        )
-        self.assertEqual(
-            len(m1._cells[0]),
-            num_rows,
-        )
+
         for i in range(num_cols):
             for j in range(num_rows):
                 self.assertEqual(
