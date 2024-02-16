b503886424dfe7230b1dac29e74a28aed22aca15
danilogalisteu <danilogalisteu@gmail.com>
2024-02-16 00:08:55 -0300

    new test for cleared visited flag; test size reduced to avoid RecursionError

--- tests.py
+++ tests.py
@@ -20,9 +20,9 @@ class Tests(unittest.TestCase):
         )
 
     def test_maze_create_cells_large(self):
-        num_cols = 80
-        num_rows = 60
-        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        num_cols = 40
+        num_rows = 30
+        m1 = Maze(0, 0, num_rows, num_cols, 20, 20)
         self.assertEqual(
             len(m1._cells),
             num_cols,
@@ -31,6 +31,25 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+
+    def test_maze_clear_visited(self):
+        num_cols = 14
+        num_rows = 10
+        m1 = Maze(50, 50, num_rows, num_cols, 50, 50, seed=42)
+        self.assertEqual(
+            len(m1._cells),
+            num_cols,
+        )
+        self.assertEqual(
+            len(m1._cells[0]),
+            num_rows,
+        )
+        for i in range(num_cols):
+            for j in range(num_rows):
+                self.assertEqual(
+                    m1._cells[i][j].visited,
+                    False,
+                )
 
 
 if __name__ == "__main__":
